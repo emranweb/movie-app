a25fb7a0208309fa1946b2a2a4aa10a14a46f39b
emranweb <emranbd77@gmail.com>
2020-07-22 23:19:42 +0600

    file organize

--- js/index.js
+++ js/index.js
@@ -17,25 +17,22 @@ async function fetchData(input) {
 }
 
 
-
-
+// auto complete search opttion
 createAutoComplete({
     element: document.querySelector(".search-wrapper"),
     renderOption: (movie) => {
-        let image = movie.Poster === "N/A" ? "https://dummyimage.com/80x114/000/eb2828&text=No+Image" : movie.Poster
+   let image = movie.Poster === "N/A" ? "https://dummyimage.com/80x114/000/eb2828&text=No+Image" : movie.Poster
 
-
-        return `<img class="poster"src="${image}"alt="">
-     <div class="suggetion-info">
-     <h4 class="title"> ${movie.Title}</h4>
-     <span class="year">Year: ${movie.Year}</span>
-     </div>`;
+   return `<img class="poster"src="${image}"alt="">
+        <div class="suggetion-info">
+        <h4 class="title"> ${movie.Title}</h4>
+        <span class="year">Year: ${movie.Year}</span>
+        </div>`;
     }
 })
 
 
-
-
+// single movie select option
 
 async function onMovieSelect(movie) {
     const response = await axios.get("http://www.omdbapi.com/", {
